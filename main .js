0f5aa177a9dd14893f117a2a67974a8c7c62fc7e
Nemanja1923 <134064705+Nemanja1923@users.noreply.github.com>
2023-05-19 05:33:47 -0700

    Add files via upload
    Dodavanje projekta

---
+++ main .js
@@ -0,0 +1,14 @@
+const panels = documet.querySelectorAll(".panel");
+
+panels.forEach((panel) => {
+	panel.addEventListener('click', () => {
+		removeActiveClass();
+		panel.classList.add("active");
+	});
+});
+	const removeActiveClass = function() {
+		panels.forEach((panel) => {
+			panel.classList.remove("active");
+		});
+		
+};
